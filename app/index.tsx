54ad0a07d2c21b2a1b111832973e33889c282d71
sisu suominen <sisu.suominen@tuni.fi>
2026-03-07 14:48:49 +0200

    Incorporating react-native-paper, buttons added

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,21 @@
-import Button from "@mui/material/Button";
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
+let counter = 0;
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text style = {styles.text}>Mjäy oon toi kissa!</Text>
-      <Button variant="contained">Contained</Button>
+      <Text style = {styles.text}>Mjäy oon tää kissa!</Text>
       <Link href="/userHistory" style={styles.button}> Activity </Link>
+
+      <Button buttonColor="#04527c" onPress={() => counter++}
+            mode="contained">Exercise done!</Button>
+
+      <Button textColor="#054364" onPress={() => counter--} mode="outlined">Oops! Never mind (-1)</Button>
+
+      <Text>Sessions completed: {+counter}</Text>
     </View>
   );
 }
@@ -17,7 +25,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#71b6c2",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   text: {
     color: "#ffffff"
