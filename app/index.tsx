774c8f3edd0b34a04ab62b0f0f0eb81c3ee69761
sisu suominen <sisu.suominen@tuni.fi>
2026-03-07 14:55:01 +0200

    Counter working with useState

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,25 @@
 import { Link } from "expo-router";
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-paper";
 
 let counter = 0;
 export default function App() {
+
+  const [counter, setCounter] = useState(0);
+  const addToCounter = () => setCounter(counter+1);
+  const removeFromCounter = () => setCounter(counter-1);
+
   return (
 
     <View style={styles.container}>
       <Text style = {styles.text}>Mjäy oon tää kissa!</Text>
       <Link href="/userHistory" style={styles.button}> Activity </Link>
 
-      <Button buttonColor="#04527c" onPress={() => counter++}
+      <Button buttonColor="#04527c" onPress={addToCounter}
             mode="contained">Exercise done!</Button>
 
-      <Button textColor="#054364" onPress={() => counter--} mode="outlined">Oops! Never mind (-1)</Button>
+      <Button textColor="#054364" onPress={removeFromCounter} mode="outlined">Oops! Never mind (-1)</Button>
 
       <Text>Sessions completed: {+counter}</Text>
     </View>
